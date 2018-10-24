3ca10c02cb8646f59be5ab566b310b6435c1fadc
zerinahuang <huangsis@student.hu-berlin.de>
2018-10-24 11:42:54 +0200

    change the layout

--- ui.R
+++ ui.R
@@ -4,21 +4,25 @@ library(leaflet)
 header <- dashboardHeader(title = "Asian Food in Berlin")
 body <- dashboardBody(
     fluidRow(
-        box(width = 8, leafletOutput("map", height = 600)),
-        column(width = 4,
-               box(selectInput("type", label = "Type of Asian Food", 
+        tags$style(type="text/css",
+                   ".shiny-output-error { visibility: hidden; }",
+                   ".shiny-output-error:before { visibility: hidden; }"
+        ),
+        box(width = 10, leafletOutput("map", height = 600)),
+        column(width = 2,
+               box(width = NULL,selectInput("type", label = "Type of Asian Food", 
                                choices = c("Chinese food" = 1, "Japanese food" = 2, "Korean food" = 3, "Vietnamese food" = 4), selected = 2)),
-               box(radioButtons("rating", label = "Rating of Food", choices = c("Poisonous" = 1,
+               box(width = NULL,radioButtons("rating", label = "Rating of Food", choices = c("Poisonous" = 1,
                                                                                       "edible" = 2,
                                                                                       "Acceptable" = 3,
                                                                                       "Joyful" = 4,
                                                                                       "Strongly recommended" = 5), selected = 3)),
                
-               br(), br(), br(), br(), br(), br(), br(), br(), br(), br(), br(),
-               br(), br(), br(), br(), br(), br(), br(), br(), br(), br(), br(),
+              
+               br(), br(), br(), br(), br(), 
               
                
-               box(title = "Comments", textOutput("text"))
+               box(width = NULL,title = "Comments", textOutput("text"))
                )
         
         
